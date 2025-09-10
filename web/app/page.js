2fce68327b70aaa84f87fb0669d18aeb8fa9edb3
Kshitij Varma <kshitijvarma@JDMB0102018KshitijtV.local>
2025-09-10 15:40:16 +0530

    removed

--- web/app/page.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="space-y-6">
-      <meta httpEquiv="refresh" content="0; url=/generate" />
-    </div>
-  );
-}
