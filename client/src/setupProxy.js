a12ff524e82124a2bc52e0c26046712d724cd326
kstij1 <kshitij@weam.ai>
2025-09-12 13:34:05 +0530

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   const target = process.env.BACKEND_PORT
     ? `http://localhost:${process.env.BACKEND_PORT}`
-    : 'http://localhost:3004';
+    : 'http://localhost:3009';
 
   app.use(
     ['/api', '/api/**'],
