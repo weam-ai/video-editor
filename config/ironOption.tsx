176f5990077fbdc21971e26ada1db2c6d3994a2e
Kshitij Varma <kshitijvarma@JDMB0102018KshitijtV.local>
2025-11-05 18:28:52 +0530

    fix pass

--- config/ironOption.tsx
+++ config/ironOption.tsx
@@ -1,6 +1,6 @@
 const ironOption = {
     cookieName: 'weam', // Must match WEAM iron-session cookie
-    password: process.env.NEXT_PUBLIC_COOKIE_PASSWORD || 'eNfUm7mmU2tIrG7fl0zTmswH7ibarfLo',
+    password: process.env.NEXT_PUBLIC_COOKIE_PASSWORD || 'YczgOhDJQj0RRDR3ASnvOVoQUBV0PtSzYczgOhDJQj0RRDR3ASnvOVoQUBV0PtSz',
     cookieOptions: {
         httpOnly: true,
         secure: false,
--- middleware.ts
+++ middleware.ts
@@ -12,6 +12,7 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)"],
 };
